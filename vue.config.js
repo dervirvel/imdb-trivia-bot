8f670963a0c18991674ba21782c76226ee6d0f22
Edward Kreutzarek <43575542+dervirvel@users.noreply.github.com>
2020-07-30 09:34:05 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@
 module.exports = {
   devServer: {
 	  proxy: 'https://localhost:8888',
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/imdb-trivia-bot/'
-    : '/'
+  publicPath: '/vue-deploy/'
   }
 }
